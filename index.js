f25c2e5262bed9d85495a658ee16aaeb4dc18f10
Matias <mattkramm@hotmail.com>
2022-08-24 22:54:07 -0300

    added embed image and updated information

--- index.js
+++ index.js
@@ -27,26 +27,39 @@ bot.on('message', async message => {
         const userProfile = await client.profiles.fetch(accountId);
         const user2v2Wins = userProfile["2v2"].win;
         const user1v1Wins = userProfile["1v1"].win;
-        const userTotalMatches = userProfile.server_data.matches_played;
-        const userTotalWins = userProfile.server_data.stat_trackers.TotalWins;
+        const userTotalRingouts = userProfile.server_data.stat_trackers.TotalRingouts;
+        const userTotalDoubleRingouts = userProfile.server_data.stat_trackers.TotalDoubleRingouts;
 
         const profileEmbed = new Discord.MessageEmbed()
         .setColor(0x6b0505)
         .setTitle('Welcome!')
         .setDescription(`These are your stats ${slicedMessage}`)
+        .setThumbnail('https://yt3.ggpht.com/PQpX2KJAsaq8Q7vWiGtNIMofLDZ0oLsiYg7tOitvqDItNNOy_0dqBNdmdhQlFbCRTx9x-3RrWQ=s900-c-k-c0x00ffffff-no-rj')
         .addFields(
-            { name: "Total matches", value: userTotalMatches, inline: true},
-            { name: "Total wins", value: userTotalWins, inline: true }
-        )
-        .addFields(
-            { name: '1v1 wins', value: user1v1Wins, inline: true},
-            { name: '2v2 wins', value: user2v2Wins },
+            { name: '1v1 wins', value: user1v1Wins, inline: true },
+            { name: '2v2 wins', value: user2v2Wins, inline: true},
+            { name: '\u200B', value: '\u200B' },
+            { name: 'Ringouts', value: userTotalRingouts, inline: true},
+            { name: 'Double Ringouts', value: userTotalDoubleRingouts, inline: true }
         )
     
         message.channel.send(profileEmbed);
-            console.log(userProfile);
+        // console.log(userProfile)
+
     } else if(message.content.startsWith("++++{}")){
         message.channel.send("That account doesn't exist");
     }
-})
+});
+
+// async function getInfoTesting(username) {
+//     const userId = await client.profiles.search(username);
+//     const accountId = await userId.results[0].result.account_id;
+//     const userProfile = await client.profiles.fetch(accountId);
+//     const user1v1Rank = await client.leaderboards.fetchProfile(accountId, "1v1");
+//     const user2v2Rank = await client.leaderboards.fetchProfile(accountId, "2v2");
+
+//     console.log(test);
+// }
+// getInfoTesting('ConsoleLog');
+
 
